58e562e2be6ebe0d4a50d41b12dcd12962231bc0
Gary Yeung <gary.yeung>
2017-02-16 11:16:38 -0800

    Adding DEV webpack file with css-loader (taking parts from create-inferno-app)

---
+++ webpack.config.js
@@ -0,0 +1,68 @@
+const webpack = require('webpack');
+const CleanWebpackPlugin = require('clean-webpack-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  entry: "./src/index.tsx", // Point to main file
+  output: {
+    path: __dirname + "/dist",
+    filename: "bundle.js"
+  },
+  resolve: {
+    extensions: ['.js', '.jsx', '.ts', '.tsx']
+  },
+  performance: {
+    hints: false
+  },
+  module: {
+    loaders: [
+      {
+        test: /\.tsx?$/,                          // All ts and tsx files will be process by
+        loaders: ['babel-loader', 'ts-loader'], // first babel-loader, then ts-loader
+        exclude: /node_modules/                   // ignore node_modules
+      }, {
+        test: /\.jsx?$/,                          // all js and jsx files will be processed by
+        loader: 'babel-loader',                   // babel-loader
+        exclude: /node_modules/                  // ignore node_modules
+      },
+      // "postcss" loader applies autoprefixer to our CSS.
+      // "css" loader resolves paths in CSS and adds assets as dependencies.
+      // "style" loader turns CSS into JS modules that inject <style> tags.
+      // In production, we use a plugin to extract that CSS to a file, but
+      // in development "style" loader enables hot editing of CSS.
+      {
+        test: /\.css$/,
+        use: [
+          'style-loader',
+          'css-loader?importLoaders=1',
+          'postcss-loader',
+        ]
+      },
+    ]
+  },
+  devServer: {
+    contentBase: "dist/",
+    historyApiFallback: true
+  }
+  ,
+  plugins: [
+    new HtmlWebpackPlugin(
+      {
+        template: "./src/index.html",
+        inject: "body"
+      }
+    ),
+    new CleanWebpackPlugin(
+      ["dist"], {
+        verbose: true
+      }
+    ),
+    // By default, webpack does `n=>n` compilation with entry files. This concatenates
+    // them into a single chunk.
+    new webpack.optimize.LimitChunkCountPlugin({
+      maxChunks: 1
+    }),
+    new webpack.HotModuleReplacementPlugin()
+  ]
+}
+;
